8cb721ae5cb422678becf41ace0e4e403b88a6c3
Rasmus Hansen <rasmus.hansen@keyshot.com>
2025-09-19 10:59:19 +0200

    Also drop owned objects before dropping a user

--- charts/postgres-topology-operator/local-deploy.sh
+++ charts/postgres-topology-operator/local-deploy.sh
@@ -1,2 +1,2 @@
-helm upgrade postgres-topology-operator . -n postgres-operator --set operator.enable=true --set operator.imagePullPolicy=IfNotPresent --set operator.image=ghcr.io/digizuite/postgres-topology-operator:local --install --create-namespace
+helm upgrade postgres-topology-operator . -n postgres-operator --set operator.enable=true --set operator.imagePullPolicy=IfNotPresent --set operator.image=digizuite.azurecr.io/digizuite/postgres-topology-operator:task-self-signed-certificates --install --create-namespace
 
